0c96ab5121e36b446e86372c6f467a3dcf23c07a
ahfish <ahfish@gmail.com>
2020-11-21 17:21:48 +0800

    add files

--- app/src/main/kotlin/gannaSquareAPI/domain/gannSquare/GannSquare.kt
+++ app/src/main/kotlin/gannaSquareAPI/domain/gannSquare/GannSquare.kt
@@ -9,8 +9,28 @@ interface QannSquareService {
 
 @Service
 class QannSquareServiceImpl : QannSquareService {
+
+    val gannCell : MutableMap<Int, GannCell>
+    val levelCache : MutableMap<Int, MutableMap<String, MutableList<GannCell>>>
+
     companion object {
-        private val loggerWithExplicitClass
-                = getLogger(QannSquareServiceImpl::class.java)
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val log = getLogger(javaClass.enclosingClass)
     }
+
+    init {
+        gannCell = mutableMapOf()
+        levelCache = mutableMapOf()
+        log.info("initialize QannSquareService")
+        var level = 0
+        val firstGann = GannCell(1,level++ )
+
+        (2..500).forEach {base ->
+            val edgeMax = level * 2
+            val levelGannCell = levelCache.putIfAbsent(level, mutableMapOf())
+//            if ( )
+
+        }
+    }
+
 }
--- app/src/main/kotlin/gannaSquareAPI/domain/gannSquare/Model.kt
+++ app/src/main/kotlin/gannaSquareAPI/domain/gannSquare/Model.kt
@@ -9,22 +9,108 @@ open class GannCell(
         open val previous: GannCell? = null
 )
 
-data class CrossCell(
+open class UpCell(
         override val base: Int,
         override val level: Int,
         override val next: GannCell? = null,
         override val previous: GannCell? = null
 ) : GannCell(base, level, next, previous)
 
-data class DiagonalCell(
+open class LeftCell(
         override val base: Int,
         override val level: Int,
         override val next: GannCell? = null,
         override val previous: GannCell? = null
 ) : GannCell(base, level, next, previous)
 
+open class RightCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : GannCell(base, level, next, previous)
 
-data class GannSquareResult(
+open class DownCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : GannCell(base, level, next, previous)
+
+open class CrossCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : GannCell(base, level, next, previous)
+
+open class DiagonalCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : GannCell(base, level, next, previous)
+
+open class UpRightDiagonalCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : DiagonalCell(base, level, next, previous)
+
+open class DownRightDiagonalCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : DiagonalCell(base, level, next, previous)
+
+open class UpLeftDiagonalCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : DiagonalCell(base, level, next, previous)
+
+
+open class DownLeftDiagonalCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : DiagonalCell(base, level, next, previous)
+
+open class UpCorssCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : CrossCell(base, level, next, previous)
+
+open class RightCorssCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : CrossCell(base, level, next, previous)
+
+open class LeftCorssCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : CrossCell(base, level, next, previous)
+
+open class DownCorssCell(
+        override val base: Int,
+        override val level: Int,
+        override val next: GannCell? = null,
+        override val previous: GannCell? = null
+) : CrossCell(base, level, next, previous)
+
+
+
+open class GannSquareResult(
         val upTrendLevel: List<BigDecimal>,
         val downTrendLevel: List<BigDecimal>
 )
